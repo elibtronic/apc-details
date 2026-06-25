b31e46008b1b5307b8faf8a92254dfac0dc1e27b
Tim Ribaric <tim.ribaric@gmail.com>
2026-06-25 13:58:18 -0400

    dropping sql, mostly function

--- src/index.py
+++ src/index.py
@@ -1,95 +1,129 @@
 import streamlit as st
-import pandas as pd
-import sqlite3
 import requests
-import json
+import pandas as pd
 
 
-def journal_details(issn):   
+### Google Sheets
+
+PUB_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRPFcOiXgZAo6XENgRXj3FoQ_BnbsYtAJq0QtlnhjGmpIkQjIp8eZNX6C66tcaooh1pfaUR8AULfSji/pub?gid=1143475194&single=true&output=csv"
+JOURNAL_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRPFcOiXgZAo6XENgRXj3FoQ_BnbsYtAJq0QtlnhjGmpIkQjIp8eZNX6C66tcaooh1pfaUR8AULfSji/pub?gid=0&single=true&output=csv"
+
+PREAMBLE = """
+# Article Processing Charge Agreements
+Below are details about what Article Processing Charge (APC) discounts and waivers are available to members of the Brock community. 
+More details can be found on the library site [:link:](https://brocku.ca/library/open-access/open-access-investments/)
+
+**NB: Data is still being finalized and results are still incomplete.**
+
+_Information Last Updated: May, 19, 2026_
+"""
+
+
+###
+
+
+
+#### Functions need to manipulate data
+
+
+@st.cache_data(ttl=3600)
+def get_data(JOURNAL_URL, PUB_URL):
+
+	#combined data
+    journalDF = pd.read_csv(JOURNAL_URL)
+    pub_DF = pd.read_csv(PUB_URL)
+    combined_DF = pd.merge(journalDF,pub_DF, on="Publisher")
+    pub_DF["Publisher Description"] = "**"+pub_DF["Publisher"] + "** [:link:](" + pub_DF["pubUrl"]+")"
+    del pub_DF["pubUrl"]
+    pub_DF.columns = ["Publisher","Discount","Publisher Description"]
+    combined_DF.columns = ["Publisher","Title","ISSN","Confidence","Publisher URL","Publisher Discount"]
+    return combined_DF, pub_DF
+
+
+def get_openalex_journal(issn,confidence):
+
     detail_string = ""
-    j_item = requests.get("https://api.openalex.org/sources/issn:"+issn).json()
+
+    if len(issn) == 8:
+        issn = issn[0:4]+"-"+issn[4:]
+
     try:
+        j_item = requests.get("https://api.openalex.org/sources/issn:"+issn).json()
 
-        if not j_item['is_oa']:
-            detail_string += " - Title is _closed_ and might be eligible for discount. Please see [journal homepage]("+j_item['homepage_url']+")"
-        else: 
-            detail_string += " - Could not determine if title is eligible for discount, or title might already be open access. Please see [journal homepage]("+j_item['homepage_url']+")"
-        
+        if confidence != 1:
+	        if not j_item['is_oa']:
+	            detail_string += " - Title is not considered _Open Access_ and  may be eligible for discount. :arrow_right: Please check _Publisher Discount_ for specifications and see [journal homepage]("+j_item['homepage_url']+") to ensure discount applies."
+	        else: 
+	            detail_string += " - Title is considered _Open Access_ or _Hybrid_ and may be eligible for disount. :arrow_right: Please check _Publisher Discount_ for specifications and see [journal homepage]("+j_item['homepage_url']+") to ensure discount applies."
+
         if j_item["apc_usd"]: 
-            detail_string += "\n- Usual Author Processing Charge for this title is: **"+str(j_item["apc_usd"])+"** USD"
+            detail_string += "\n - Usual Article Processing Charge for this title is: **"+str(j_item["apc_usd"])+"** USD"
 
-        detail_string += "\n- More analytics for this title from [OpenAlex]("+j_item['ids']['openalex']+")"
+        detail_string += "\n - More analytics for this title from [OpenAlex]("+j_item['ids']['openalex']+")"
     except:
         detail_string = "**Could not retrieve extra journal information**"
     return detail_string
 
 
-
-@st.cache_data(ttl=3600)
-def get_publisher_info():
-	conn = sqlite3.connect("apc-details.db")
-	pub_info = pd.read_sql_query("SELECT * from publisherInfo",con=conn)
-	conn.close()
-	pub_info["Publisher"] = "**"+pub_info["Publisher"] + "** [:link:](" + pub_info["pubUrl"]+")"
-	del(pub_info["pubUrl"])
-	pub_info.columns = ["Publisher", "Discount Details"]
-	return pub_info
-
-@st.cache_data(ttl=3600)
-def get_apc_info():
-	conn = sqlite3.connect("apc-details.db")
-	apc_info = pd.read_sql_query("SELECT * from apcDetails",con=conn)
-	apc_info.sort_values(by=["Journal_Title"],inplace=True)
-	conn.close()
-	return apc_info
+####
 
 
-#st.image("brock_logo.png",width=200)
-st.title("APC Discount and Waivers")
-st.write("""Below are details about what Article Processing Charge (APC) discounts and waivers are available to members of the Brock community. 
-	        More details can be found on the library site [:link:](https://brocku.ca/library/open-access/open-access-investments/)""")
-st.write("**NB: Data is still being finalized and results are still incomplete.**")
-st.write("""_Information Last Updated: May, 19, 2026_""")
+
+#### Load data using cache function
+combined_DF, pub_DF = get_data(JOURNAL_URL,PUB_URL)
 
 
-journalTab, pubTab = st.tabs(['Journal','Publisher'])
+#### Render Page
+st.image("logo.png",width=200)
+st.write(PREAMBLE)
+
+
+journalTab, pubTab = st.tabs(['By Journal','Publisher'])
 
 
 with pubTab:
-	st.write("The following is general information discounts and waivers by publisher.")
-	pub_info = get_publisher_info()
-	st.table(pub_info)
+	st.table(pub_DF[["Publisher Description","Discount"]])
+
 
 with journalTab:
-
-	apc_info = get_apc_info()
-	pubSelect = st.selectbox(label="Select a Publisher to Narrow", index=None, options=apc_info["Publisher"].sort_values(ascending=True).unique())
+	pubSelect = st.selectbox(label="Select a publisher to narrow", index=None, options=combined_DF["Publisher"].sort_values(ascending=True).unique())
 
 	if pubSelect:
-		infoshow = apc_info[apc_info["Publisher"] == pubSelect]
-		st.write("Details for: "+str(pubSelect))
+		infoshow = combined_DF[combined_DF["Publisher"] == pubSelect]
+		st.write("General publisher discount details:")
+		pub_details = pub_DF[pub_DF["Publisher"] == pubSelect]["Discount"].iloc[0]
+		st.write(" - "+pub_details)
 
-		#pub_info["Publisher"] == pubSelect
+		st.write("_Select a journal title for more information_")
+
+		event = st.dataframe(infoshow[["Title","ISSN"]],on_select="rerun",selection_mode="single-row")
+		#st.dataframe(infoshow[["Title","ISSN"]],hide_index=True)
+		st.write("Total titles for this publisher: ",len(infoshow))
+	else:
+		infoshow = combined_DF
+		st.write("_Journal Titles_")
+		event = st.dataframe(infoshow[["Title","ISSN"]],on_select="rerun",selection_mode="single-row")
+		st.write("Total titles for all publishers: ",len(infoshow))
+
+	if event.selection.rows:
+
+		j_details = {
+
+		"Title": infoshow.iloc[event.selection.rows[0]]["Title"],
+		"ISSN" : infoshow.iloc[event.selection.rows[0]]["ISSN"],
+		"Publisher": infoshow.iloc[event.selection.rows[0]]["Publisher"],
+		"Confidence": infoshow.iloc[event.selection.rows[0]]["Confidence"],
+		"Publisher Discount": infoshow.iloc[event.selection.rows[0]]["Publisher Discount"]
+		}
+
+		j_details["Additional Information"] = get_openalex_journal(j_details["ISSN"],j_details["Confidence"])
+		del j_details["Confidence"] #comment back out for diagnostic info
+
+		st.table(j_details)
 		
 
-		st.write("_Select Title for more information_")
-		event = st.dataframe(infoshow[["Journal_Title"]],on_select="rerun",selection_mode="single-row")
-		st.write("_Titles total for publisher: "+ str(len(infoshow))+"_")
-	else:
-		infoshow = apc_info
-		event = st.dataframe(infoshow[["Journal_Title","Publisher","pubDiscount"]],on_select="rerun",selection_mode="single-row")
-		st.write("_Titles total for all publishers: "+ str(len(infoshow))+"_")
 
-	if event.selection.rows:		
-		#st.dataframe(infoshow.iloc[event.selection.rows[0]])
-		st.markdown("--------")
-		st.markdown("")
-		st.markdown(" **Journal Title:** "+infoshow.iloc[event.selection.rows[0]]['Journal_Title'])
-		st.markdown("**Publisher:** "+infoshow.iloc[event.selection.rows[0]]['Publisher'])
-		st.markdown("**Typical Publisher Discount:** "+infoshow.iloc[event.selection.rows[0]]['pubDiscount'])
-		st.markdown("--------")
-		st.markdown("**Extra Details:** ")
-		st.markdown(journal_details(infoshow.iloc[event.selection.rows[0]]['ISSN']))
+
 
 
 
